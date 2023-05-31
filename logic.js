4c5216a7abe9e3a68a46f6a946ec261f887a8e03
Shreyash Kumar <shreyashgaur1@gmail.com>
2023-05-31 20:03:03 +0530

    Whack A Mole game.

---
+++ logic.js
@@ -0,0 +1,45 @@
+
+const cells = document.querySelectorAll('.cells');
+const scoreBoard = document.querySelector('#score');
+const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+function randomHole(cells) {
+    const idx = Math.floor(Math.random() * cells.length);
+    const hole = cells[idx];
+    if (hole === lastHole) {
+        console.log('Ah nah thats the same one bud');
+        return randomHole(cells);
+    }
+    lastHole = hole;
+    return hole;
+}
+function peep() {
+    const time = randomTime(200, 1000);
+    const hole = randomHole(cells);
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove('up');
+        if (!timeUp) peep();
+    }, time);
+}
+function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    // alert('game started')
+    peep();
+    setTimeout(() => timeUp = true, 10000)
+}
+function bonk(e) {
+    if (!e.isTrusted) return;
+    score++;
+    this.parentNode.classList.remove('up');
+    scoreBoard.textContent = score;
+}
+moles.forEach(mole => mole.addEventListener('click', bonk));
